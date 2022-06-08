e4134b5de3f1b39f0a7616e21915890b2918b54f
the-armistead <armistead2000@gmail.com>
2022-06-08 15:29:44 -0500

    two more

---
+++ Js/Grasshopper - Check for factor.js
@@ -0,0 +1,6 @@
+//MY SOLUTION 
+let checkForFactor = (base, factor) => base % factor === 0 ? true : false;
+//ALTERNATIVE
+function checkForFactor (base, factor) {
+    return base % factor === 0;
+  }
