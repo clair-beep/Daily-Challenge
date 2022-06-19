dbcc037de95ae78244aa59ea0fc317f5b1177dcf
clair <armistead2000@gmail.com>
2022-06-19 18:51:58 -0500

    Adding  a string and a number, will result in a string

--- 7 Sku/Square Every Digit.js
+++ 7 Sku/Square Every Digit.js
@@ -1,2 +1,7 @@
 // My solution 
+const squareDigits = num => +num.toString().split("").map(x => x ** 2).join("");
 //alternative
+function squareDigits(num){
+    return Number(('' + num).split('').map(function (val) { return val * val;}).join(''));
+    
+  }
