ebf73371f5dd5dafcf35f963029660717d91af6f
clair <armistead2000@gmail.com>
2022-06-19 19:59:02 -0500

    Filter seems more straight forward

--- 7 Sku/Disemvowel Trolls.js
+++ 7 Sku/Disemvowel Trolls.js
@@ -1,3 +1,16 @@
 //my solution
-
+function disemvowel(str) {
+    let count = 0;
+    let vowels = ["a", "e", "i", "o", "u"];
+    return str.split("").filter(item => !vowels(item))
+     
+  }
 //alternative 
+const vowels = 'aeiou';
+
+function disemvowel(str) {
+  return str
+    .split('')
+    .filter(letter => !vowels.includes(letter.toLowerCase()))
+    .join('');
+}
