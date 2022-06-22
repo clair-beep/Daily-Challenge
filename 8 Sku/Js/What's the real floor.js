3e8e11305bbeb4063a29a4b279f248e6238434c0
clair <armistead2000@gmail.com>
2022-06-22 09:24:11 -0500

    Ternary operator

--- 8 Sku/Js/What's the real floor.js
+++ 8 Sku/Js/What's the real floor.js
@@ -1,3 +1,16 @@
 //MY SOLUTION
-
+function getRealFloor(n) {
+    if(n===1) {
+      return 0;
+    } else if(n <= 0){
+      return n;
+    }else if (n < 13) {
+      return n-1;
+    } else {
+      return n-2;
+    }
+  }
 //ALTERNATIVE
+function getRealFloor(n) {
+    return n > 13 ? n - 2 : n > 0 ? n - 1 : n;
+  }
