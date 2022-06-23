e218e6b44ecd562af62bed6035327af1226f23de
clair <armistead2000@gmail.com>
2022-06-23 08:52:58 -0500

    AZccording to some  comments the uses of arr methods cra)Ns resourses

--- 8 Sku/Js/My head is at the wrong end!.js
+++ 8 Sku/Js/My head is at the wrong end!.js
@@ -1,3 +1,8 @@
 //MY SOLUTION
+function fixTheMeerkat(arr) {
 
+    return[arr.pop(),arr[1], arr.shift()];
+      
+   }
 //ALTERNATIVE
+const fixTheMeerkat = ([tail, body, head]) => [head, body, tail]
