c399e3fb296bf2de2b9dc847e77eda3fb699652e
clair <armistead2000@gmail.com>
2022-06-25 11:56:23 -0500

    split enables you to make a string an array

--- 7 Sku/Game Hit the target.js
+++ 7 Sku/Game Hit the target.js
@@ -4,3 +4,7 @@ const solution = mtrx =>{
     return arr.indexOf('>') < arr.indexOf('x') 
   }
 //MY SOLUTION
+const solution = mtrx =>{ 
+    const arr = mtrx.filter(el => el.includes('>') && el.includes('x') )[0] || []
+    return arr.indexOf('>') < arr.indexOf('x') 
+  }
---
+++ 8 Sku/Js/Remove String Spaces.js
@@ -0,0 +1,4 @@
+// ALTERNATIVE
+function noSpace(x){return x.split(' ').join('')}
+//MY SOLUTION
+const noSpace = x =>  x.split(' ').join("");
