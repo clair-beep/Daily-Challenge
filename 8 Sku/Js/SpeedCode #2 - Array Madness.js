65f289b3021accfc7e5b13c8aa841cca7fa0d883
clair <armistead2000@gmail.com>
2022-06-26 10:43:17 -0500

    practice with reduce

--- 8 Sku/Js/SpeedCode #2 - Array Madness.js
+++ 8 Sku/Js/SpeedCode #2 - Array Madness.js
@@ -1,3 +1,6 @@
 // ALTERNATIVE
- 
+const arrayMadness = (a, b) => a.reduce((acc, x) => acc + x**2, 0) > b.reduce((acc, x) => acc + x**3, 0) 
 //MY SOLUTION
+function arrayMadness(a, b) {
+    return a.reduce((previousValue, currentValue) => previousValue + currentValue ** 2, 0) > b.reduce((previousValue, currentValue) => previousValue + currentValue ** 3, 0);
+    }
