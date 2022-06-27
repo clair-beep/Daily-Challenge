be473ac6427438689d5e19c29fdd0d0bed849ef4
clair <armistead2000@gmail.com>
2022-06-27 09:22:48 -0500

    A number needs to be a string in order to use split

--- 8 Sku/Convert number to reversed array of digits.js
+++ 8 Sku/Convert number to reversed array of digits.js
@@ -1,3 +1,6 @@
 // ALTERNATIVE
-
+function digitize(n) {
+    return Array.from(String(n), Number).reverse();
+  }
 //MY SOLUTION
+const digitize = n => String(n).split("").map((num)=>Number(num)).reverse();
