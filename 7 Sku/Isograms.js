99ca765fdf5281e96eb25ed40d6c93459d3b4c56
clair <armistead2000@gmail.com>
2022-06-29 10:02:44 -0500

    Introduction of set

--- 7 Sku/Isograms.js
+++ 7 Sku/Isograms.js
@@ -1,3 +1,17 @@
 //MY SOLUTION
-
+function isIsogram(str){
+    let i, j;
+    str = str.toLowerCase();
+    for(i = 0; i < str.length; ++i) {
+      for(j = i + 1; j < str.length; ++j) {
+        if(str[i] === str[j]) {
+          return false;
+        }
+      }
+    }
+    return true;
+ }
 //ALTERNATIVE
+function isIsogram(str){
+    return new Set(str.toUpperCase()).size == str.length;
+  }
