b0f6d9908dc8a5e2612e683a6896a83b9ba5b213
clair <armistead2000@gmail.com>
2022-06-29 11:14:31 -0500

    The use of repeat and n

---
+++ 7 Sku/Baby shark lyrics generator.js
@@ -0,0 +1,7 @@
+//solution
+
+function babySharkLyrics(){
+    names = ["Baby shark", "Mommy shark", "Daddy shark", "Grandma shark", "Grandpa shark", "Let's go hunt"];
+    return names.map((name)=> `${name},${" doo".repeat(6)}\n`.repeat(3) + `${name}!\n`)+ "Run away,…";
+  }
+console.log(babySharkLyrics(""));
