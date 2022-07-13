3a766f3836985519d5619b19921045bcec0f414d
clair <armistead2000@gmail.com>
2022-07-12 21:07:04 -0500

    First commit

--- 7 Sku/Flatten and sort an array.js
+++ 7 Sku/Flatten and sort an array.js
@@ -1,3 +1,12 @@
 //MY SOLUTION
 
+function flattenAndSort(array) {
+    return [].concat(...array).sort((a,b) => a - b);
+  }
 //ALTERNATIVE
+function flattenAndSort(array) {
+    return array
+      .reduce((result, current) => [...result, ...current],[])
+      .sort((a, b) => a - b)
+      ;
+  }
