09923ccffb40140546406aedbe34e30bd237fe5f
clair <armistead2000@gmail.com>
2022-07-15 09:49:30 -0500

    set up

---
+++ 7 Sku/Divide and Conquer.js
@@ -0,0 +1,28 @@
+//ALTERNATIVE
+
+//MY SOLUTION 
+// parameteres mixed stings and num in an array 
+
+//  return the diference from num  - string 
+// return will be a num 
+
+// examples: [9, 3, '7', '3']), 2); 
+// num: 9, 3
+// striNg: '7', '3'
+// result: num - string = 2
+
+// create reparate variables for string 
+// create reparate variables for nums
+// rest 
+// return 
+
+
+
+function divCon(x){
+    let string = x.filter(element => typeof element === 'string').reduce(
+      (x, y) => +x + +y,0);
+    let nums = x.filter(element => typeof element === 'number').reduce(
+      (x, y) => +x + +y,0);
+    
+      return nums - string;
+    }
