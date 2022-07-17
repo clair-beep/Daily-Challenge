38ae5dec91c2531524095ea07c7d277dbc60b969
clair <armistead2000@gmail.com>
2022-07-17 06:50:11 -0900

    practice from a previous exercise

---
+++ 7 Sku/Money, Money, Money.js
@@ -0,0 +1,58 @@
+// my solution 
+//we will get 4: 
+
+//principal
+// interest, 
+//tax 
+// desired
+
+//we will need to return the amount of years it will take to for the person to obtain the amount desired 
+// amount will be years 
+// 0 can be a valuable num 
+
+
+/* example: 
+Let P be the Principal = 1000.00      
+  Let I be the Interest Rate = 0.05      
+  Let T be the Tax Rate = 0.18      
+  Let D be the Desired Sum = 1100.00
+
+
+After 1st Year -->
+  P = 1041.00
+After 2nd Year -->
+  P = 1083.86
+After 3rd Year -->
+  P = 1128.30
+
+*/
+
+// CREATE A LOOP 
+// ACONDITIOAL TO EVALUATE if Desired Principal 'D' is equal to Principal 'P' this should return 0 Years.
+function calculateYears(principal, interest, tax, desired) {
+    let time = 0; 
+    if (principal == desired) {
+        return 0;
+    } else {
+        for (let index = 0; principal <= desired; index++) {
+           
+            principal = (interest * principal + principal - interest * principal * tax);
+            console.log(principal)
+            time++
+            }
+      
+        }
+    return time;
+    }
+    
+// ALTERNATIVE 
+
+function calculateYears(principal, interest, tax, desired) {
+    // your code
+    var years = 0;
+    while(principal < desired){
+      principal += (principal * interest) * (1 - tax);
+      years++;
+    }
+    return years;
+}
