5ed43aa2fded6c193dde40d74cdf5dab5e22e02d
clair <armistead2000@gmail.com>
2022-07-21 11:07:10 -0500

    string practice

---
+++ 7 Sku/Printer Errors.js
@@ -0,0 +1,43 @@
+// MY SOLUTION 
+//pameters =>  you wyiireceive a string that can contain letters from a to m which are good ahd n to z that are considered  errors 
+
+//returns => count the num of errors and return a num 
+
+// //examples => s="aaabbbbhaijjjm"
+// printer_error(s) => "0/14"
+
+// s="aaaxbbbbyyhwawiwjjjwwm"
+// printer_error(s) => "8/22"
+
+// make a variable to define errors 
+// make str an arr 
+// loop through it 
+
+function printerError(s) {
+    let count = 0;
+    s = s.split('');
+    const errors = ['o','p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    
+        for (let error of s) {
+            if (errors.includes(error)) {
+                count++;
+            }
+        }
+    
+        // return string of errors count 
+
+        return `${count}/${s.length}`;
+}
+
+
+//ALTERNATIVE
+function printerError(s) {
+    // your code
+    var count = 0;
+    for(var i = 0; i < s.length; i++) {
+      if (s[i] > "m") {
+        count++;
+      }
+    }
+    return count+"/"+s.length;
+}
