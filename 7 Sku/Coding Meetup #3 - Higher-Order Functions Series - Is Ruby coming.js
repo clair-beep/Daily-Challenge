cc661a0d59efafcc98227288365a2853d9f7c559
clair <armistead2000@gmail.com>
2022-09-10 11:01:44 -0500

    intro to  some method

---
+++ 7 Sku/Coding Meetup #3 - Higher-Order Functions Series - Is Ruby coming.js
@@ -0,0 +1,8 @@
+//solution 
+function isRubyComing(list) {
+    return list.map(Element => Element.language).includes('Ruby')}
+    
+// alternative 
+function isRubyComing(list) {
+    return list.some(e => e.language === 'Ruby');
+  }
