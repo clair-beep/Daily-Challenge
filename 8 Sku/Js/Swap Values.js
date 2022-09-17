dcc86ca6662334839a569bf5b42c85ac21ee992e
clair <armistead2000@gmail.com>
2022-09-16 21:12:39 -0500

    replace useful to eliminate characters

---
+++ 8 Sku/Js/Swap Values.js
@@ -0,0 +1,11 @@
+function swapValues(args) {
+    return args.reverse();
+ }
+
+ //alternative
+
+//  function swapValues(args) {
+//     var temp = args[0];
+//     args[0] = args[1];
+//     args[1] = temp;
+//   }
