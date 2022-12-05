28053e42e2aef8c0b05d544920ed1346287916bc
the-armistead <armistead2000@gmail.com>
2022-12-05 18:32:55 -0500

    char code at nums

--- 7 Sku/Love vs friendship.js
+++ 7 Sku/Love vs friendship.js
@@ -1,2 +1,13 @@
 //solution 
 
+function wordsToMarks(string){
+    let result = 0;
+    for(let i = 0; i < string.length; i++){
+      result += string.charCodeAt(i) - 96
+    }
+    return result
+  }
+
+  //alternative
+
+  const wordsToMarks = s => [...s].reduce((res, c) => res += c.charCodeAt() - 96, 0)
