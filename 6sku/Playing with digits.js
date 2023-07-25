f651f5ce10d532d4a67ed3b317c1be9129f4742c
unknown <armistead2000@gmail.com>
2023-07-25 09:48:35 +0200

    Playing with digits

--- 6sku/Playing with digits.js
+++ 6sku/Playing with digits.js
@@ -1,6 +1,7 @@
 // find k
 //k == sum of the digits of n to the power of p incremental
 // final result is to find if k is divisible by p
+// Check if result is excual to the round number
 
 function digPow(n, p) {
   // ...
